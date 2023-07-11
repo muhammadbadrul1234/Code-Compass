d80a1814aeb3e2ff12c602995393f9e749f7b4ab
muhammadbadrul1234 <badrulalomtawsyat@gmail.com>
2023-07-11 22:03:21 +0600

    Login PAge Design

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
+import Login from "./Login";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <h1>Hello, world!</h1>
+      <Login />
   </React.StrictMode>
 );
