59d75e6c7242d9da9bf2f3b47b339b647609a459
muhammadbadrul1234 <badrulalomtawsyat@gmail.com>
2023-07-12 21:25:24 +0600

    Login Base Done

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-import Login from "./Signup";
 import Signup from "./Signup";
+import { Login } from "./Login";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-      <Signup />
+      <Login />
   </React.StrictMode>
 );
