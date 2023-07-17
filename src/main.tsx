573edde6f5812e4e7519b357cb86ff7d525420d4
muhammadbadrul1234 <badrulalomtwsyat@gmail.com>
2023-07-17 22:05:27 +0600

    slight edit

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
-import Signup from "./Signup";
 import { Login } from "./Login";
 import { Signup } from "./components/Signup";
 
