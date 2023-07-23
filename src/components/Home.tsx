4ed2d17b573d51423f49edba3f843b6bf1a90839
muhammadbadrul1234 <badrulalomtwsyat@gmail.com>
2023-07-24 02:33:17 +0600

    minor edit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Navbar } from "./Navbar";
+import connects from "./db";
 
 export const Home = () => {
+  
   return (
+    
     <>
       <Navbar />
+      
+
       
     </>
   );
